686f25e3efb543d1a60c7b7c633a626c3d87ebb8
g.hashimoto <egghershey@gmail.com>
2025-04-06 09:29:01 +0900

    first commit

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'account' => 'Lorem ipsum dolor ',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'name' => 'Lorem ipsum dolor ',
+                'email' => 'Lorem ipsum dolor sit amet',
+                'tel' => 'Lorem ipsum dolor ',
+                'deleted' => '2025-04-06 09:28:26',
+                'created' => 1743899306,
+                'modified' => 1743899306,
+                'created_user' => 'Lorem ipsum dolor sit amet',
+                'modified_user' => 'Lorem ipsum dolor sit amet',
+            ],
+        ];
+        parent::init();
+    }
+}
